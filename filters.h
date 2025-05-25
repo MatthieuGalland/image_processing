1b35150e678734265066355d5f987778acf71207
thomas <thomas.darbo@efrei.net>
2025-05-25 23:35:27 +0200

    adding readme.md

---
+++ filters.h
@@ -0,0 +1,49 @@
+//
+// Created by thoma on 25/05/2025.
+//
+
+#ifndef FILTERS_H
+#define FILTERS_H
+
+
+const float BOX_BLUR[3][3] = {
+    {1.f/9, 1.f/9, 1.f/9},
+    {1.f/9, 1.f/9, 1.f/9},
+    {1.f/9, 1.f/9, 1.f/9}
+};
+
+
+const float GAUSSIAN_BLUR[3][3] = {
+    {1.f/16*1, 1.f/16*2, 1.f/16*1},
+    {1.f/16*2, 1.f/16*4, 1.f/16*2},
+    {1.f/16*1, 1.f/16*2, 1.f/16*1}
+};
+
+const float GAUSSIAN_BLUR_SIMPLE[3][3] = {
+    {1.f/16, 2.f/16, 1.f/16},
+    {2.f/16, 4.f/16, 2.f/16},
+    {1.f/16, 2.f/16, 1.f/16}
+};
+
+
+const int OUTLINE[3][3] = {
+    {-1, -1, -1},
+    {-1,  8, -1},
+    {-1, -1, -1}
+};
+
+
+const int EMBOSS[3][3] = {
+    {-2, -1,  0},
+    {-1,  1,  1},
+    { 0,  1,  2}
+};
+
+
+static const int SHARPEN[3][3] = {
+    { 0, -1,  0},
+    {-1,  5, -1},
+    { 0, -1,  0}
+};
+
+#endif //FILTERS_H
